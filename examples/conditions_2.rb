633098a27cbb6d9c91ed421a2735fb0e9fa6baab
daglus <daglus.ck.ua@gmail.com>
2019-05-27 17:27:11 +0300

    Episode #7. Conditional expressions

---
+++ examples/conditions_2.rb
@@ -0,0 +1,27 @@
+i = 1
+
+# if i > 1
+#   puts 'number is gratet then 1'
+# elsif i < 1
+#   puts 'number is less than 1'
+# else
+#   puts 'number is quals 1'
+# end
+
+if i >= 1
+  puts 'number is gratet then 1'
+elsif i <= 1
+  puts 'number is less than 1'
+else
+  puts 'number is quals 1'
+end
+
+puts '--------'
+
+if i >= 1
+  puts 'number is gratet then 1'
+end
+
+if i <= 1
+  puts 'number is less than 1 or quals 1'
+end
