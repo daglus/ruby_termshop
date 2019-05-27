7a7f5bbfd8cdae12945698ad8ce149dc381ba4fc
daglus <daglus.ck.ua@gmail.com>
2019-05-27 18:11:10 +0300

    Episode #33. Reflection: method missing. Storeapp.

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -12,3 +12,10 @@ require_relative "order"
 @items << AntiqueItem.new({:price => 101, :weight => 100, :name => "car"})
 @items << RealItem.new({:price => 101, :weight => 100, :name => "kettle"})
 @items << RealItem.new({:price => 101, :weight => 100, :name => "dishwasher"})
+
+cart = Cart.new('kolya')
+cart.add_item RealItem.new({:price => 101, :weight => 100, :name => "car"})
+cart.add_item RealItem.new({:price => 150, :weight => 100, :name => "car"})
+cart.add_item RealItem.new({:price => 170, :weight => 100, :name => "kettle"})
+p cart.all_cars
+p cart.all_kettles
