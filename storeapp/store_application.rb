cc53812fdf1ce350972e297234df3e69cedc1c36
daglus <daglus.ck.ua@gmail.com>
2019-05-27 18:15:16 +0300

    Episode #36. Singletons and classes methods. Storeapp.

---
+++ storeapp/store_application.rb
@@ -0,0 +1,19 @@
+class StoreApplication
+  class << self
+    def new
+      unless @instance
+        puts 'Loading files..'
+        puts self.class
+        require_relative "string"
+        require_relative "item_container"
+        require_relative "item"
+        require_relative "virtual_item"
+        require_relative "real_item"
+        require_relative "antique_item"
+        require_relative "cart"
+        require_relative "order"
+      end
+      @instance ||= self
+    end
+  end
+end
