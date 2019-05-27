8dd8a26f216cf98244bf73bdb1b9164b9265a9b0
daglus <daglus.ck.ua@gmail.com>
2019-05-27 17:37:08 +0300

    Episode #15. Symbols, hashes and setting properties. Storeapp.

--- storeapp/item.rb
+++ storeapp/item.rb
@@ -1,16 +1,14 @@
 class Item
 
-  def initialize
-    @price = 30
+  def initialize(options)
+    @price = options[:price]
+    @weight = options[:weight]
   end
-  #attr_accessor :price, :weight
+
   attr_reader :price, :weight
   attr_writer :price
 end
 
-item1 = Item.new
-item1.price = 10
+item1 = Item.new({ :weight => 10, :price => 30})
 puts item1.price
-
-item1.weight = 15
 puts item1.weight
