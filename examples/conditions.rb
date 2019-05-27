d5fa9f54765889028202c49fb73b8fca7850fc38
daglus <daglus.ck.ua@gmail.com>
2019-05-27 17:26:50 +0300

    Episode #6. Conditional expressions

---
+++ examples/conditions.rb
@@ -0,0 +1,22 @@
+if 2 > 1
+  puts 'Hello world!'
+end
+
+if true
+  puts 'Hello true world!'
+end
+
+name = 'John'
+if name == 'James'
+  puts 'My name is Bond.'
+end
+
+name = 'John'
+if name == 'James'|| 2 > 1
+  puts 'My name is Bond.'
+end
+
+name = 'John'
+unless name == 'James' && 2 > 1
+  puts 'My name is Bond.'
+end
