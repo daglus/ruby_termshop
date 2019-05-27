23fe14ab3f30f3422c61ec004e66fee57ca18405
daglus <daglus.ck.ua@gmail.com>
2019-05-27 19:09:26 +0300

    Episode #37. DSL: Domain Specific Language. Storeapp. Add .env.

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -1,7 +1,20 @@
 require_relative "store_application"
 
-puts StoreApplication.new == StoreApplication.new
-p StoreApplication.new
+StoreApplication.config do |app|
+  app.name = 'My store'
+  app.environment = :production
+
+  app.admin do |admin|
+    admin.email = ENV['ADMIN_EMAIL']
+    admin.login = ENV['ADMIN_LOGIN']
+    admin.send_info_emails_on :mondays
+  end
+end
+
+p StoreApplication.environment
+p StoreApplication.name
+p StoreApplication::Admin.email
+p StoreApplication::Admin.login
 
 @items = []
 @items << AntiqueItem.new("car", price: 101, weight: 100)
@@ -16,4 +29,4 @@ cart.add_item RealItem.new({:price => 150, :weight => 100, :name => "car"})
 cart.add_item RealItem.new({:price => 170, :weight => 100, :name => "kettle"})
 
 method = 'all_cars'
-p cart.send(method)
+# p cart.send(method)
--- storeapp/store_application.rb
+++ storeapp/store_application.rb
@@ -1,9 +1,12 @@
+require 'dotenv/load'
 class StoreApplication
+
   class << self
-    def new
+
+    def config
       unless @instance
+        yield(self)
         puts 'Loading files..'
-        puts self.class
         require_relative "string"
         require_relative "item_container"
         require_relative "item"
@@ -15,5 +18,30 @@ class StoreApplication
       end
       @instance ||= self
     end
+
+    attr_accessor :name, :environment
+
+    def admin(&block)
+      @admin ||= Admin.new(&block)
+    end
+
   end
+
+  class Admin
+    class << self
+      def new
+        unless @instance
+          yield(self)
+        end
+        @instance ||= self
+      end
+
+      attr_accessor :email, :login
+
+      def send_info_emails_on(day)
+        @send_info_emails_on = day
+      end
+    end
+  end
+
 end
