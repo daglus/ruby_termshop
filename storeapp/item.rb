9931875ed58fd3c9e570931b4e2e3269614754e7
daglus <daglus.ck.ua@gmail.com>
2019-05-27 18:05:52 +0300

    Episode #29. Reopening classes and automatic type conversions. Storeapp.

--- storeapp/item.rb
+++ storeapp/item.rb
@@ -26,6 +26,10 @@ class Item
     (@real_price - @real_price*self.class.discount) + tax if @real_price
   end
 
+  def to_s
+    "#{self.name}:#{self.price}:#{self.weight}"
+  end
+
   private
 
   def tax
