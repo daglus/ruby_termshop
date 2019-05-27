0ba1015cc07faf6a2d2df8a002976c1c3550af23
daglus <daglus.ck.ua@gmail.com>
2019-05-27 17:35:31 +0300

    Episode #14. Attribute readers, writers and accessors. Storeapp.

--- storeapp/item.rb
+++ storeapp/item.rb
@@ -1,20 +1,16 @@
 class Item
+
   def initialize
     @price = 30
   end
-
-  def price
-    @price
-  end
-
-  def price=(price_value)
-    @price = price_value
-  end
+  #attr_accessor :price, :weight
+  attr_reader :price, :weight
+  attr_writer :price
 end
 
 item1 = Item.new
-puts item1.price
 item1.price = 10
 puts item1.price
-item1.price = 20
-puts item1.price
+
+item1.weight = 15
+puts item1.weight
