0e5ec32a266a04c8e0bd21659ab602bbb1305411
daglus <daglus.ck.ua@gmail.com>
2019-05-27 18:13:38 +0300

    Episode #35. Syntax sugar. Storeapp.

--- storeapp/init.rb
+++ storeapp/init.rb
@@ -1,17 +1,19 @@
 require_relative "string"
 require_relative "item_container"
 require_relative "item"
-require_relative "real_item"
 require_relative "virtual_item"
+require_relative "real_item"
 require_relative "antique_item"
 require_relative "cart"
 require_relative "order"
 
 
 @items = []
-@items << AntiqueItem.new({:price => 101, :weight => 100, :name => "car"})
+@items << AntiqueItem.new("car", price: 101, weight: 100)
 @items << RealItem.new({:price => 101, :weight => 100, :name => "kettle"})
 @items << RealItem.new({:price => 101, :weight => 100, :name => "dishwasher"})
+
+@items.each { |i| puts i.name }
 
 cart = Cart.new('kolya')
 cart.add_item RealItem.new({:price => 101, :weight => 100, :name => "car"})
