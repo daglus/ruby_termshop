77a9c375972f8593501df59d83e869381e7c9624
daglus <daglus.ck.ua@gmail.com>
2019-05-28 11:03:39 +0300

    Episode #46. Rspec: Nested describe and pending tests. Storeapp.

--- storeapp/spec/cart_spec.rb
+++ storeapp/spec/cart_spec.rb
@@ -6,11 +6,20 @@ require_relative '../app/item_container'
 require_relative '../app/cart'
 
 describe Cart do
-  it 'add items into to cart' do
-    cart = Cart.new('kolya')
-    item1 = Item.new('kettle', price: 200)
-    item2 = Item.new('car', price: 200)
-    cart.add_items(item1, item2)
-    cart.items.should include(item1, item2)
-  end  
+
+  describe 'managing items' do
+    it 'add items into to cart' do
+      cart = Cart.new('kolya')
+      item1 = Item.new('kettle', price: 200)
+      item2 = Item.new('car', price: 200)
+      cart.add_items(item1, item2)
+      cart.items.should include(item1, item2)
+    end
+
+    it 'removes items  form itself'
+  end
+
+  it 'count items in itself'
+  it 'places order using all the items that where added into the cart'
+  it 'clear itself of the items after an order is placed'
 end
