844ba85e4e5cc1b74d3049d75ea06dbb89d3ddf1
daglus <daglus.ck.ua@gmail.com>
2019-05-28 20:12:49 +0300

    Episode #48-49. Proc and lambda. Storeapp.

---
+++ storeapp/spec/real_item_spec.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require_relative '../app/item'
+require_relative '../app/virtual_item'
+require_relative '../app/real_item'
+
+describe RealItem do
+  it 'uses weight while returning info if the condition is true' do
+    item1 = RealItem.new(name: 'kettle', price: 200, weight: 2)
+    item2 = RealItem.new(name: 'kettle', price: 200, weight: 10)
+    item1_info = []
+    item2_info = []
+    item1.info { |attr| item1_info << attr }
+    item2.info { |attr| item2_info << attr }
+    item1_info.join(', ').should == '232.0, kettle'
+    item2_info.join(', ').should == '10, 232.0, kettle'
+  end
+end
